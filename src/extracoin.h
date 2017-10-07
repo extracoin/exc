bd910d569601799abae444accd6c3941d3d95945
online089 <32590256+online089@users.noreply.github.com>
2017-10-07 10:24:17 +0200

    Update and rename CryptoNoteConfig.h to extracoin.h

--- src/extracoin.h
+++ src/extracoin.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2011-2017 The ExtraCOIN developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -7,7 +7,7 @@
 #include <cstdint>
 #include <initializer_list>
 
-namespace CryptoNote {
+namespace ExtraCOIN {
 namespace parameters {
 
 const uint64_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
